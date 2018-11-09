dcf8b4180553e75c7c89363e54a20fa7b63b60ce
jbo-ads <jerome.je.boue.external@astrium.eads.net>
2018-11-09 12:57:20 +0100

    Allow package selection at installation time

--- genyum.sh
+++ genyum.sh
@@ -26,28 +26,37 @@ REPOCONF_EOF
 cd ..
 tar czf $tmpname.tgz $tmpname/
 
-cat << INSTALLYUM_EOF > installyum.sh
+cat << INSTALLDEPS_EOF > installdeps.sh
 if cat /var/log/cron >& /dev/null
 then
+  # Command line arguments specify which packages shall be installed
+  # This defaults to all packages contained in the archive
+  deps="$*"
+  test \$# -gt 0 && deps=\$*
+
+  # Find where actual archive starts and extract it
   plstart=\$(awk '/^__PAYLOAD_BELOW__/{print NR+1;exit 0;}' \$0)
   tail -n+\$plstart \$0 | tar xz -C $(dirname $TMPDIR)
+  
+  # Declare the newly installed YUM repository and use it
   cp $TMPDIR/$tmpname.repo /etc/yum.repos.d/$tmpname.repo
-  yum install -y --disablerepo=* --enablerepo=$tmpname $*
+  yum install -y --disablerepo=* --enablerepo=$tmpname \$deps
   exit 0
 else
+  # Installer needs root privileges
   echo sudo required
   exit 1
 fi
 __PAYLOAD_BELOW__
-INSTALLYUM_EOF
+INSTALLDEPS_EOF
 
-cat $tmpname.tgz >> installyum.sh
-chmod +x installyum.sh
+cat $tmpname.tgz >> installdeps.sh
+chmod +x installdeps.sh
 
 # Cleanup
 rm -rf $tmpname.tgz $tmpname
 
 # Report location of autoextracting archive
-printf "\n\nAutoextracting archive is %s\n" $(pwd)/installyum.sh
+printf "\n\nAutoextracting archive is %s\n" $(pwd)/installdeps.sh
 
 ################################################################################
